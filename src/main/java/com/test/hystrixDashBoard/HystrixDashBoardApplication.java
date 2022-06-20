3ec516cc38d6b3b5cd730b656677d875adc4087b
unknown <vamsiduvvuru999@gmail.com>
2022-06-20 22:06:32 +0530

    changes

---
+++ src/main/java/com/test/hystrixDashBoard/HystrixDashBoardApplication.java
@@ -0,0 +1,18 @@
+package com.test.hystrixDashBoard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+
+@SpringBootApplication
+@EnableHystrixDashboard
+public class HystrixDashBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HystrixDashBoardApplication.class, args);
+	}
+
+}
